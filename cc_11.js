1ec9ce0dfaa753a0b90fa74b1ad08aef954bc3d6
Eugene Rodriguez <er11@usf.edu>
2025-03-01 00:47:21 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -1,7 +1,8 @@
+// Task 1
 class Book {
     constructor(title, author, isbn, copies) {
         this.title = title; // string
-        this.auther = author; // string
+        this.author = author; // string
         this.isbn = isbn; // number
         this.copies = copies; // number
     }
@@ -11,10 +12,32 @@ class Book {
     updateCopies(quantity) {
         this.copies += quantity;
     } // update copies of book
-}
+}; // class for book
 
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails()); // Title: The Great Gatsby, Author: undefined, ISBN: 123456, Copies: 5
 
 book1.updateCopies(-1); // udating copies of book
 console.log(book1.getDetails()); // Title: The Great Gatsby, Author: undefined, ISBN: 123456, Copies: 4
+
+// Task 2
+class Borrower {
+    constructor(name, borrowerId, borrowedBooks) {
+        this.name = name; // string
+        this.borrowerId = borrowerId; // number
+        this.borrowedBooks = []; // array
+    }
+    borrowBook(book) {
+        this.borrowedBooks.push(book); // adding books to array
+    }
+    returnBook(book) {
+        this.borrowedBooks = this.borrowedBooks.filter(b => b.isbn !== book.isbn); // removing books to array
+    }
+}; // class for borrower
+
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // [ 'The Great Gatsby' ]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // []
