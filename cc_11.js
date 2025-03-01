21d83b048b54228d14f745811828e3e98966451e
Eugene Rodriguez <er11@usf.edu>
2025-03-01 00:17:20 -0500

    Task 1 - Created Book Class

---
+++ cc_11.js
@@ -0,0 +1,20 @@
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title; // string
+        this.auther = author; // string
+        this.isbn = isbn; // number
+        this.copies = copies; // number
+    }
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`
+    }; // get details of book
+    updateCopies(quantity) {
+        this.copies += quantity;
+    } // update copies of book
+}
+
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+console.log(book1.getDetails()); // Title: The Great Gatsby, Author: undefined, ISBN: 123456, Copies: 5
+
+book1.updateCopies(-1); // udating copies of book
+console.log(book1.getDetails()); // Title: The Great Gatsby, Author: undefined, ISBN: 123456, Copies: 4
